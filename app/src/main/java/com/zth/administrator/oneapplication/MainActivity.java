d49be5e4f82315ef481272c765968dbf1f4316a7
zth <1045546352@qq.com>
2016-06-24 17:02:52 +0800

    aaaa

---
+++ app/src/main/java/com/zth/administrator/oneapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.zth.administrator.oneapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //view -tool windows fanvorites 打开书签
+//F11  ctrl+f11 添加书签(Bookmarks)
+    //自动生成 alert+insert
+
+    //shift +f11 书签管理器  难道书签的内容
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void fn(){
+        //TODO  NIHAO
+
+    }
+}
